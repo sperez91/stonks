c2ce4ca0ae559a05b61f707c3e14ce3d999d01a2
llvllch <mart@llvll.ch>
2021-01-16 21:21:28 +0100

    MVP

--- cryptotick.py
+++ cryptotick.py
@@ -17,6 +17,9 @@ import os
 import yaml 
 import time
 import socket
+import numpy as np
+import matplotlib.pyplot as plt
+import currency
 
 dirname = os.path.dirname(__file__)
 configfile = os.path.join(os.path.dirname(os.path.realpath(__file__)),'config.yaml')
@@ -48,13 +51,45 @@ def print_system_info(display):
     print('  LUT version: {}'.format(epd.lut_version))
     print()
 
+def _place_text(img, text, x_offset=0, y_offset=0,fontsize=40,fontstring="Forum-Regular"):
+    '''
+    Put some centered text at a location on the image.
+    '''
+
+    draw = ImageDraw.Draw(img)
+
+    try:
+        filename = os.path.join(dirname, './fonts/'+fontstring+'.ttf')
+        font = ImageFont.truetype(filename, fontsize)
+    except OSError:
+        font = ImageFont.truetype('/usr/share/fonts/TTF/DejaVuSans.ttf', fontsize)
+
+    img_width, img_height = img.size
+    text_width, _ = font.getsize(text)
+    text_height = fontsize
+
+    draw_x = (img_width - text_width)//2 + x_offset
+    draw_y = (img_height - text_height)//2 + y_offset
+
+    draw.text((draw_x, draw_y), text, font=font,fill=(0,0,0) )
+
+def writewrappedlines(img,text,fontsize,y_text=-300,height=110, width=27,fontstring="Forum-Regular"):
+    lines = textwrap.wrap(text, width)
+    for line in lines:
+        width= 0
+        _place_text(img, line,0, y_text, fontsize,fontstring)
+        y_text += height
+    return img
+
+def clear_display(display):
+    print('Clearing display...')
+    display.clear()
+
 def beanaproblem(image,message):
 #   A visual cue that the wheels have fallen off
     thebean = Image.open(os.path.join(picdir,'thebean.bmp'))
-    draw = ImageDraw.Draw(image)
     image.paste(thebean, (60,15))
-    draw.text((15,150),message, font=font_date,fill = 0)
-    image = ImageOps.mirror(image)
+    writewrappedlines(image, message,fontsize=80, y_text=150,height=100,width=40 ,fontstring="Forum-Regular")
     return image 
 
 def display_image_8bpp(display, img):
@@ -79,22 +114,22 @@ def getData(config):
     endtime = int(time.time())
     starttime = endtime - 60*60*24*days_ago
     starttimeseconds = starttime
-    endtimeseconds = endtime     
+    endtimeseconds = endtime
+    allprices = {}
     # Get the price 
     for whichcoin in crypto_list:
         print(whichcoin)
         geckourl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency="+fiat+"&ids="+whichcoin
         print(geckourl)
         rawlivecoin = requests.get(geckourl).json()
-        logging.info(rawlivecoin[0])   
+        print(rawlivecoin[0])   
         liveprice = rawlivecoin[0]
         pricenow= float(liveprice['current_price'])
-        pricenow= float(liveprice['last'])
-        logging.info("Got Live Data From CoinGecko")
+        print("Got Live Data From CoinGecko")
         geckourlhistorical = "https://api.coingecko.com/api/v3/coins/"+whichcoin+"/market_chart/range?vs_currency="+fiat+"&from="+str(starttimeseconds)+"&to="+str(endtimeseconds)
         logging.info(geckourlhistorical)
         rawtimeseries = requests.get(geckourlhistorical).json()
-        logging.info("Got price for the last "+str(days_ago)+" days from CoinGecko")
+        print("Got price for the last "+str(days_ago)+" days from CoinGecko")
         timeseriesarray = rawtimeseries['prices']
         timeseriesstack = []
         length=len (timeseriesarray)
@@ -102,113 +137,94 @@ def getData(config):
         while i < length:
             timeseriesstack.append(float (timeseriesarray[i][1]))
             i+=1
-
         timeseriesstack.append(pricenow)
-        allprices={whichcoin: timeseriesstack}
+        allprices[whichcoin] = timeseriesstack
     return allprices
 
-def makeSpark(pricestack):
+def makeSpark(allprices):
     # Draw and save the sparkline that represents historical data
 
     # Subtract the mean from the sparkline to make the mean appear on the plot (it's really the x axis)    
-    x = pricestack-np.mean(pricestack)
+    for key in allprices.keys():   
+        x = allprices[key]-np.mean(allprices[key])
 
-    fig, ax = plt.subplots(1,1,figsize=(10,3))
-    plt.plot(x, color='k', linewidth=6)
-    plt.plot(len(x)-1, x[-1], color='r', marker='o')
+        fig, ax = plt.subplots(1,1,figsize=(10,3))
+        plt.plot(x, color='k', linewidth=3)
+        plt.plot(len(x)-1, x[-1], color='r', marker='o')
 
-    # Remove the Y axis
-    for k,v in ax.spines.items():
-        v.set_visible(False)
-    ax.set_xticks([])
-    ax.set_yticks([])
-    ax.axhline(c='k', linewidth=4, linestyle=(0, (5, 2, 1, 2)))
+        # Remove the Y axis
+        for k,v in ax.spines.items():
+            v.set_visible(False)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.axhline(c='k', linewidth=2, linestyle=(0, (5, 2, 1, 2)))
 
-    # Save the resulting bmp file to the images directory
-    plt.savefig(os.path.join(picdir,'spark.png'), dpi=17)
-    imgspk = Image.open(os.path.join(picdir,'spark.png'))
-    file_out = os.path.join(picdir,'spark.bmp')
-    imgspk.save(file_out) 
-    plt.clf() # Close plot to prevent memory error
+        # Save the resulting bmp file to the images directory
+        plt.savefig(os.path.join(picdir, key+'spark.png'), dpi=72)
+        plt.clf() # Close plot to prevent memory error
 
 
-def updateDisplay(config,pricestack):
+def updateDisplay(image,config,allprices):
     """   
     Takes the price data, the desired coin/fiat combo along with the config info for formatting
     if config is re-written following adustment we could avoid passing the last two arguments as
     they will just be the first two items of their string in config 
     """
+    crypto_list = currencystringtolist(config['ticker']['currency'])
+    fiat_list=currencystringtolist(config['ticker']['fiatcurrency'])
+    fiat=fiat_list[0]
     days_ago=int(config['ticker']['sparklinedays'])   
     symbolstring=currency.symbol(fiat.upper())
+
     if fiat=="jpy":
         symbolstring="¥"
-    pricenow = pricestack[-1]
-    currencythumbnail= 'currency/'+whichcoin+'.bmp'
-    tokenfilename = os.path.join(picdir,currencythumbnail)
-    sparkbitmap = Image.open(os.path.join(picdir,'spark.bmp'))
-#   Check for token image, if there isn't one, get on off coingecko, resize it and pop it on a white background
-    if os.path.isfile(tokenfilename):
-        logging.info("Getting token Image from Image directory")
-        tokenimage = Image.open(tokenfilename)
-    else:
-        logging.info("Getting token Image from Coingecko")
-        tokenimageurl = "https://api.coingecko.com/api/v3/coins/"+whichcoin+"?tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false"
-        rawimage = requests.get(tokenimageurl).json()
-        tokenimage = Image.open(requests.get(rawimage['image']['large'], stream=True).raw)
-        resize = 100,100
-        tokenimage.thumbnail(resize, Image.ANTIALIAS)
-        new_image = Image.new("RGBA", (120,120), "WHITE") # Create a white rgba background with a 10 pizel border
-        new_image.paste(tokenimage, (10, 10), tokenimage)   
-        tokenimage=new_image
-        tokenimage.thumbnail((100,100),Image.ANTIALIAS)
-        tokenimage.save(tokenfilename)
+    height=160
 
+    heightincrement=295
+    for key in allprices.keys():  
+        pricenow = allprices[key][-1]
+        whichcoin=key
+        currencythumbnail= 'currency/'+whichcoin+'.png'
+        tokenfilename = os.path.join(picdir,currencythumbnail)
+        sparkpng = Image.open(os.path.join(picdir,key+'spark.png'))
+    #   Check for token image, if there isn't one, get on off coingecko, resize it and pop it on a white background
+        if os.path.isfile(tokenfilename):
+            logging.info("Getting token Image from Image directory")
+            tokenimage = Image.open(tokenfilename)
+        else:
+            print("Getting token Image from Coingecko")
+            tokenimageurl = "https://api.coingecko.com/api/v3/coins/"+whichcoin+"?tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false"
+            rawimage = requests.get(tokenimageurl).json()
+            tokenimage = Image.open(requests.get(rawimage['image']['large'], stream=True).raw)
+            tokenimage = tokenimage.convert("RGBA")
+            new_image = Image.new("RGBA", (290,290), "WHITE") # Create a white rgba background with a 10 pizel border
+            new_image.paste(tokenimage, (20, 20), tokenimage)   
+            tokenimage=new_image
+            tokenimage.save(tokenfilename)
+        newsize=(200,200)
+        tokenimage.thumbnail(newsize,Image.ANTIALIAS) 
 
-    pricechange = str("%+d" % round((pricestack[-1]-pricestack[0])/pricestack[-1]*100,2))+"%"
-    if pricenow > 1000:
-        pricenowstring =format(int(pricenow),",")
-    else:
-        pricenowstring =str(float('%.5g' % pricenow))
+        pricechange = str("%+d" % round((allprices[key][-1]-allprices[key][0])/allprices[key][-1]*100,2))+"%"
+        if pricenow > 1000:
+            pricenowstring =format(int(pricenow),",")
+        else:
+            pricenowstring =str(float('%.5g' % pricenow))
 
-    if config['display']['orientation'] == 0 or config['display']['orientation'] == 180 :
-        epd = epd2in7.EPD()
-        epd.Init_4Gray()
-        image = Image.new('L', (epd.width, epd.height), 255)    # 255: clear the image with white
-        draw = ImageDraw.Draw(image)              
-        draw.text((110,80),str(days_ago)+"day :",font =font_date,fill = 0)
-        draw.text((110,95),pricechange,font =font_date,fill = 0)
-        # Print price to 5 significant figures
-        draw.text((15,200),symbolstring+pricenowstring,font =font,fill = 0)
-        draw.text((10,10),str(time.strftime("%H:%M %a %d %b %Y")),font =font_date,fill = 0)
-        image.paste(tokenimage, (10,25))
-        image.paste(sparkbitmap,(10,125))
-        if config['display']['orientation'] == 180 :
-            image=image.rotate(180, expand=True)
-
-
-    if config['display']['orientation'] == 90 or config['display']['orientation'] == 270 :
-        epd = epd2in7.EPD()
-        epd.Init_4Gray()
-        image = Image.new('L', (epd.height, epd.width), 255)    # 255: clear the image with white
         draw = ImageDraw.Draw(image)   
-        draw.text((110,90),str(days_ago)+"day : "+pricechange,font =font_date,fill = 0)
+ #       draw.text((110,90),str(days_ago)+"day : "+pricechange,font =font_date,fill = 0)
         # Print price to 5 significant figures
- #       draw.text((20,120),symbolstring,font =fonthiddenprice,fill = 0)
-        draw.text((10,120),symbolstring+pricenowstring,font =fontHorizontal,fill = 0)
-        image.paste(sparkbitmap,(80,40))
-        image.paste(tokenimage, (0,10))
-        draw.text((95,15),str(time.strftime("%H:%M %a %d %b %Y")),font =font_date,fill = 0)
-        if config['display']['orientation'] == 270 :
-            image=image.rotate(180, expand=True)
-#       This is a hack to deal with the mirroring that goes on in 4Gray Horizontal
-        image = ImageOps.mirror(image)
+        image.paste(sparkpng, (290,height+40))
+        image.paste(tokenimage, (120,height+30))
 
-#   If the display is inverted, invert the image usinng ImageOps        
-    if config['display']['inverted'] == True:
-        image = ImageOps.invert(image)
-#   Send the image to the screen        
-    epd.display_4Gray(epd.getbuffer_4Gray(image))
-    epd.sleep()
+        text=symbolstring+pricenowstring
+        _place_text(image, text, x_offset=470, y_offset=height-400,fontsize=120,fontstring="JosefinSans-Light")
+        text=str(days_ago)+" day : "+pricechange
+        _place_text(image, text, x_offset=470, y_offset=height-320,fontsize=60,fontstring="JosefinSans-Light")
+        height += heightincrement
+    text=str(time.strftime("%H:%M %a %d %b %Y"))
+    _place_text(image, text, x_offset=0, y_offset=-420,fontsize=50,fontstring="JosefinSans-Medium")
+    return image
+
 
 def currencystringtolist(currstring):
     # Takes the string for currencies in the config.yaml file and turns it into a list
@@ -307,9 +323,11 @@ def main():
             print("FULL UPDATE")
             allprices=getData(config)
             # generate sparkline
-#           makeSpark(allprices)
+            print("SPARKY")
+            makeSpark(allprices)
             # update display
-#           updateDisplay(config, allprices)
+            screenimage=updateDisplay(img,config, allprices)
+            display_image_8bpp(display, screenimage)
             print(allprices)
             lastgrab=time.time()
             time.sleep(.2)
