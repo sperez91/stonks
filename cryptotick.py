543f5605722d2a6e783ef764e8aaf8d324d1815f
llvllch <mart@llvll.ch>
2021-09-04 19:46:19 +0200

    Transient disabling of pandas..... undo in 10 mins

--- cryptotick.py
+++ cryptotick.py
@@ -23,7 +23,7 @@ import socket
 import numpy as np
 import matplotlib.pyplot as plt
 import currency
-import pandas as pd
+#import pandas as pd
 import logging
 import gpiozero
 from fake_useragent import UserAgent
